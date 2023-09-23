d03600065f20f953aa4d050d68b6d22109df9ced
YordanDobrev <petolisko@gmail.com>
2023-09-23 07:32:06 +0300

    ready

---
+++ Data Types and Variables - Exercise/01. Integer Operations.py
@@ -0,0 +1,10 @@
+#Read User Input
+num1, num2, num3, num4 = int(input()), int(input()), int(input()), int(input())
+
+#Logic
+
+total = int(((num1 + num2) / num3)) * num4
+
+#Print Output
+
+print(total)
