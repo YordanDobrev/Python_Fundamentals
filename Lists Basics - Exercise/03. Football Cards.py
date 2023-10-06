d39c8d4c791822cdf027e40d1604e8e107f61c58
YordanDobrev <petolisko@gmail.com>
2023-10-06 18:41:42 +0300

    ready

---
+++ Lists Basics - Exercise/03. Football Cards.py
@@ -0,0 +1,23 @@
+#Read User Input
+team_a = ['A-1', 'A-2', 'A-3', 'A-4', 'A-5', 'A-6', 'A-7', 'A-8', 'A-9', 'A-10', 'A-11']
+team_b = ['B-1', 'B-2', 'B-3', 'B-4', 'B-5', 'B-6', 'B-7', 'B-8', 'B-9', 'B-10', 'B-11']
+
+referee = input().split()
+game_terminated = False
+#Logic
+
+for i in referee:
+    if i in team_a:
+        team_a.remove(i)
+    elif i in team_b:
+        team_b.remove(i)
+    if len(team_a) < 7 or len(team_b) < 7:
+        game_terminated = True
+        break
+
+#Print Output
+if game_terminated:
+    print(f"Team A - {len(team_a)}; Team B - {len(team_b)}")
+    print(f"Game was terminated")
+else:
+    print(f"Team A - {len(team_a)}; Team B - {len(team_b)}")
