2aa34e5f18be304d51d2a0cc9c68be655d66f82a
YordanDobrev <petolisko@gmail.com>
2023-10-06 12:11:49 +0300

    ready

---
+++ List Basics/01. Strange Zoo.py
@@ -0,0 +1,12 @@
+#Read User Input
+first_string = input()
+second_string = input()
+third_string = input()
+
+#Logic
+
+list = [third_string, second_string, first_string]
+
+#Print Output
+
+print(list)
