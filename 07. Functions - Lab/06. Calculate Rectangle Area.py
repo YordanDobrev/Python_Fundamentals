5919590f8f9e34ddc151a30449ddd506e085a2f4
YordanDobrev <petolisko@gmail.com>
2023-10-08 14:21:47 +0300

    ready

---
+++ 07. Functions - Lab/06. Calculate Rectangle Area.py
@@ -0,0 +1,14 @@
+# Read User Input
+width = int(input())
+height = int(input())
+
+
+# Logic
+
+def rectangle(w: int, h: int) -> int:
+    return w * h
+
+
+# Print Output
+
+print(rectangle(width, height))
