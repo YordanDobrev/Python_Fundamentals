9fa4a5f28b0a5c720fe098e01413e6533f5720e1
YordanDobrev <petolisko@gmail.com>
2023-10-11 15:08:31 +0300

    ready

---
+++ 08. Functions - Exercise/01. Smallest of Three Numbers.py
@@ -0,0 +1,17 @@
+def smallest(n1, n2, n3):
+    inputs = n1, n2, n3
+    return min(inputs)
+
+
+# Read User Input
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+
+# Logic
+
+
+# Print Output
+
+print(smallest(first_num, second_num, third_num))
---
+++ More Exercises/Lists - Lab/03. Sum Adjacent Equal Numbers.py
@@ -0,0 +1,16 @@
+# Read User Input
+list = input().split()
+
+# Logic
+
+new_list = []
+
+for i in list:
+    current_value = int(i)
+    if current_value == current_value + 1:
+        new_list_
+
+
+# Print Output
+
+print(type(list[0]))
