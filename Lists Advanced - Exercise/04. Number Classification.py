158f02b7a76a349542eb2d7291b349c72ff2a58b
YordanDobrev <petolisko@gmail.com>
2023-10-14 15:58:39 +0300

    ready

---
+++ Lists Advanced - Exercise/04. Number Classification.py
@@ -0,0 +1,17 @@
+# Read User Input
+numbers = input().split(", ")
+
+# Logic
+
+positive = [num for num in numbers if int(num) >= 0]
+negative = [num for num in numbers if int(num) < 0]
+even = [num for num in numbers if int(num) % 2 == 0]
+odd = [num for num in numbers if int(num) % 2 != 0]
+
+# Print Output
+
+print(f'Positive: {", ".join(positive)}')
+print(f'Negative: {", ".join(negative)}')
+print(f'Even: {", ".join(even)}')
+print(f'Odd: {", ".join(odd)}')
+
