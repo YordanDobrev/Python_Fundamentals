48d382df22d7506672824a8a8db70ca998f80a6f
YordanDobrev <petolisko@gmail.com>
2023-10-30 16:06:52 +0200

    ready

---
+++ More Exercises/Lists - Lab/04_josephus_permutation.py
@@ -0,0 +1,16 @@
+# Read User Input
+# the_group = "1 2 3 4 5 6 7".split()  # input().split()
+# sequence = 3  # int(input())
+#
+# # Logic
+#
+# new_list = []
+# counter = 0
+# for i in range(1, len(the_group) + 1_pr):
+#     current_number = int(the_group[i])
+#
+#     if current_number % 3 == 0:
+#         new_list.append(current_number)
+#
+# # Print Output
+# print(new_list)
