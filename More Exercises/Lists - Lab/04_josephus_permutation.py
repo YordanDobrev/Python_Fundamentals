11a58875814fe5aa9dc8b3f0819e72e0799e9fff
YordanDobrev <petolisko@gmail.com>
2023-11-14 10:20:31 +0200

    ready

--- More Exercises/Lists - Lab/04_josephus_permutation.py
+++ More Exercises/Lists - Lab/04_josephus_permutation.py
@@ -1,16 +1,19 @@
-# Read User Input
-# the_group = "1 2 3 4 5 6 7".split()  # input().split()
-# sequence = 3  # int(input())
-#
-# # Logic
-#
-# new_list = []
-# counter = 0
-# for i in range(1, len(the_group) + 1_pr):
-#     current_number = int(the_group[i])
-#
-#     if current_number % 3 == 0:
-#         new_list.append(current_number)
-#
-# # Print Output
-# print(new_list)
+circle = input().split()
+skip = int(input())
+
+new_list = []
+counter = 0
+
+index = 0
+while len(circle) > 0:
+    counter += 1
+
+    if counter % skip == 0:
+        new_list.append(int(circle.pop(index)))
+    else:
+        index += 1
+
+    if index >= len(circle):
+        index = 0
+
+print(str(new_list).replace(' ', '').replace('\'', ''))
